779deff882b5f251c7cd5f7732af7896abc4941b
ehud560 <132360462+ehud560@users.noreply.github.com>
2023-06-21 09:35:39 +0300

    Delete contact.js

--- contact.js
+++
@@ -1,9 +0,0 @@
-// retreive the info from the contact page.
-
-const urlParams = new URLSearchParams(window.location.search);
-
-// Data from the fields.
-document.getElementById("submittedName").textContent = urlParams.get("name");
-document.getElementById("submittedEmail").textContent = urlParams.get("email");
-document.getElementById("submittedSubject").textContent = urlParams.get("subject");
-document.getElementById("submittedMessage").textContent = urlParams.get("message");
